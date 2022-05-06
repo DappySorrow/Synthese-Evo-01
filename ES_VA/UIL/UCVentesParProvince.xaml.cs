c5a85e78b2828d00b70913a16cf7d71d68770d6c
DappySorrow <j.si.barbeau8@gmail.com>
2022-05-06 16:34:33 -0400

    Continuer la selection de dates pour que se soit smooth

--- ES_VA/UIL/UCVentesParProvince.xaml.cs
+++ ES_VA/UIL/UCVentesParProvince.xaml.cs
@@ -126,6 +126,22 @@ namespace UIL
                 }
             }
 
+            //Si la selection est vide
+            if (cbAnneesFin.SelectedIndex == -1)
+            {
+                cbAnneesFin.SelectedItem = newAnnees.ToArray()[0];
+            }
+            //Si la selection n'est pas vide
+            else
+            {
+                //Si la selection est trop grande
+                if (int.Parse(cbAnneesFin.SelectedItem.ToString()) > int.Parse(cbAnneesDebut.SelectedItem.ToString()))
+                {
+                    cbAnneesFin.SelectedItem = newAnnees.ToArray()[0];
+                }
+
+            }
+
             cbAnneesFin.ItemsSource = newAnnees;
         }
 
